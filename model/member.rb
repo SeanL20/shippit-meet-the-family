41af9b62e7f15f5aafb12179855796c55306c858
Sean Lim <seanl30@yahoo.com>
2020-11-13 19:03:55 +1000

    Title: Add New Family Tree Data CSV & Update Some Fields.
    Status: Complete.

--- model/member.rb
+++ model/member.rb
@@ -1,12 +1,12 @@
 require_relative 'parents'
 
 Class Member
-	attr_acessor :name, :gender, :spouse
+	attr_acessor :name, :gender, :married_to
 
-	def initialize(name, gender, spouse, parents)
+	def initialize(name, gender, married_to, parents)
 		@name = name
 		@gender = gender
-		@spouse = spouse
+		@married_to = married_to
 		@parents = parents
 	end
 end
