60427fbc2453996d760ccf73fc423ed8e259c4aa
Sean Lim <seanl30@yahoo.com>
2020-11-15 00:28:38 +1000

    Title: Streamline The Parent Data By Having It In The Member Model Instead.
    Status: Complete.

--- model/member.rb
+++ model/member.rb
@@ -1,13 +1,12 @@
-require_relative 'parents'
+class Member
+	attr_accessor :name, :gender, :married_to, :father_name, :mother_name
 
-Class Member
-	attr_acessor :name, :gender, :married_to
-
-	def initialize(name, gender, married_to, parents)
+	def initialize(name, gender, married_to, father_name, mother_name, family_tree)
 		@name = name
 		@gender = gender
 		@married_to = married_to
-		@parents = parents
+		@father_name = father_name
+		@mother_name = mother_name
 		@family_tree = family_tree
 
 		@family_tree.member << self
--- model/parents.rb
+++
@@ -1,8 +0,0 @@
-Class Parents
-	attr_acessor :father_name, :mother_name
-
-	def initialize(father_name, mother_name)
-		@father_name = father_name
-		@mother_name = mother_name
-	end
-end
