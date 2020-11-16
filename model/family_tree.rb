88052c372b49f6f72b512977c2975bf29e864c3f
Sean Lim <seanl@presentplate.com>
2020-11-16 14:22:30 +1000

    Title: Add Some Functions For Getting The Mother, Father, Members With Same Parents For Siblings & Getting The Son.
    Status: Complete.

--- model/family_tree.rb
+++ model/family_tree.rb
@@ -9,14 +9,70 @@ class FamilyTree
 		@family_members = []
 	end
 
-	def find_by_name(name)
+	def find_by_name(member_name)
 		family_members.each do |fam_mem|
 			# check if the family member name is the same as the parameter before returning nil or the family member.
-			if fam_mem.name == name
+			if fam_mem.name == member_name
 				return fam_mem
 			end
 		end
 		return nil
+	end
+
+	def get_mother(member_name)
+		member = find_by_name(member_name)
+		family_members.each do |fam_mem|
+			# check if the family member name is the same as the parameter before returning nil or the family member.
+			if fam_mem.name == member.mother_name
+				return fam_mem
+			end
+		end
+		return nil
+	end
+
+	def get_father(member_name)
+		member = find_by_name(member_name)
+		family_members.each do |fam_mem|
+			# check if the family member name is the same as the parameter before returning nil or the family member.
+			if fam_mem.name == member.father_name
+				return fam_mem
+			end
+		end
+		return nil
+	end
+
+	def get_members_with_same_parents(member_name)
+		member = find_by_name(member_name)
+		member_array = []
+		family_members_without_self = family_members
+		family_members_without_self.delete(member)
+		family_members_without_self.each do |fam_mem|
+			# check if the family member name is the same as the parameter before returning nil or the family member.
+			if fam_mem.father_name == member.father_name && fam_mem.mother_name == member.mother_name
+				member_array << fam_mem
+			end
+		end
+		return member_array
+	end
+
+	def get_siblings(member_name)
+		member_array = get_members_with_same_parents(member_name)
+		if member_array != []
+			return member_array.map { |e| e.name }.join(" ")
+		else
+			return "NONE"			
+		end
+	end
+
+	def get_son(member_name)
+		member_array = []
+		family_members.each do |fam_mem|
+			# check if the family member name is the same as the parameter before returning nil or the family member.
+			if fam_mem.father_name == member.name || fam_mem.mother_name == member.name
+				member_array << fam_mem
+			end
+		end
+		return member_array
 	end
 
 	def import(csv_data)
