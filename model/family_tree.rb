083c4ce6b41ffafe6d2350ef4b1aee8dbefd531a
Sean Lim <seanl@presentplate.com>
2020-11-16 11:46:11 +1000

    Title: Add Comments.
    Status: Complete.

--- model/family_tree.rb
+++ model/family_tree.rb
@@ -11,6 +11,7 @@ class FamilyTree
 
 	def find_by_name(name)
 		family_members.each do |fam_mem|
+			# check if the family member name is the same as the parameter before returning nil or the family member.
 			if fam_mem.name == name
 				return fam_mem
 			end
@@ -20,6 +21,7 @@ class FamilyTree
 
 	def import(csv_data)
 		CSV.foreach(csv_data, :headers => true) do |data|
+			# Create New Members From Each Line Of The CSV.
 			Member.new(data["Name"], data["Gender"], data["Married_To"], data["Father_Name"], data["Mother_Name"], self)
 		end
 	end
